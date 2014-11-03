74af15b8e00705c34f3a3cd31429adcf3b472cd9
Spammofist <rene.fritzsch.1el@fh-zwickau.de>
2014-11-03 11:14:28 +0100

    Finish log in/log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,33 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  # Returns the hash gigest of the gien string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Retuns a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistant sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Retuns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forget a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+
   resources :users
 
   root 'main#home'
 
-  match '/signup',    to: 'users#new',        via: 'get'
-
-  match '/home',      to: 'main#home',        via: 'get'
-  match '/info/impressum', to: 'info#impressum',   via: 'get'
-
-
+  match '/signup',           to: 'users#new',             via: 'get'
+  match '/home',             to: 'main#home',             via: 'get'
+  match '/info/impressum',   to: 'info#impressum',        via: 'get'
+  match '/login',            to: 'sessions#new',          via: 'get'
+  match '/login',            to: 'sessions#create',       via: 'post'
+  match '/logout',           to: 'sessions#destroy',      via: 'delete'
 
  end
